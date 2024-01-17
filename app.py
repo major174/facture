b1f2e01663b064f98ef9b1cb8009c9601db2be28
12alain <kiemdealain02@gmail.com>
2024-01-17 00:21:22 +0000

    updte

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ col1, col2, col3, col4 = st.columns(4)
 current_date = datetime.now().strftime("%d-%m-%Y")
 
 with col1:
-    designation = st.text_input("Désignation")
+    designation = st.text_input("Désignation",key="text")
 
 with col2:
-    quantite_vendue_text = st.text_input("Quantité Vendue")
+    quantite_vendue_text = st.text_input("Quantité Vendue",key="vend")
 
 with col3:
-    prix_vente_unitaires = st.text_input("Prix de Vente Unitaire")
+    prix_vente_unitaires = st.text_input("Prix de Vente Unitaire",key="unit")
     prix_vente_unitaire = int(prix_vente_unitaires) if prix_vente_unitaires else 0
 
 with col4:
@@ -46,19 +46,33 @@ with col4:
     quantite_vendue = int(quantite_match.group()) if quantite_match else 0
     quantite_vendue = int(quantite_vendue) if quantite_vendue else 0
     prix_vente_total = quantite_vendue * prix_vente_unitaire
-
-if st.button("Ajouter"):
-    if designation and quantite_vendue is not None and prix_vente_unitaire is not None:
-        st.session_state.data_list.append({
-            "DESIGNATION": designation,
-            "QUANTITE VENDUE": quantite_vendue_text,
-            "Prix de Vente Unitaire": prix_vente_unitaire,
-            "Prix de Vente Total": prix_vente_total
-        })
-
 df_updated = pd.DataFrame(st.session_state.data_list)
-st.write("Facture", df_updated)
+def clear_text():
+    st.session_state["text"] = ""
+    st.session_state["vend"] = ""
+    st.session_state["unit"] = ""
+add_button_column, button_column = st.columns(2)
 
+       
+with add_button_column:
+    if st.button("Ajouter"):
+        if len(df_updated) == 30:
+            st.warning("Veuillez enregistrer cette facture car vous avez atteint la limite. Une fois qu'on appuie sur le bouton 'Ajouter', il n'ajoute plus de données.")
+        elif designation and quantite_vendue is not None and prix_vente_unitaire is not None:
+            st.session_state.data_list.append({
+                "DESIGNATION": designation,
+                "QUANTITE VENDUE": quantite_vendue_text,
+                "Prix de Vente Unitaire": prix_vente_unitaire,
+                "Prix de Vente Total": prix_vente_total
+            })
+
+            df_updated = pd.DataFrame(st.session_state.data_list)
+with button_column:
+    st.button("Effacer", on_click=clear_text)
+st.write("Facture", df_updated)
+if st.button("Effacers le tableau "):
+        st.session_state.data_list = []  # Efface la liste des données
+        st.session_state.row_to_edit = None 
 st.header("Modifier la facture")
 
 row_indices = [i for i in range(len(st.session_state.data_list))]
